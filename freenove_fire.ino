8b9b22614fcda2cb02f71446f1a1e3292e7bb230
codewitch-honey-crisis <gaztoast@gmail.com>
2025-01-18 06:40:49 -0800

    added lots of comments

--- freenove_fire.ino
+++ freenove_fire.ino
@@ -16,13 +16,22 @@
 using namespace gfx;
 using namespace uix;
 
+// make a transfer buffer big enough for 1/10th of the display
 static constexpr const size_t lcd_transfer_size = 320 * 240 * 2 / 10;
+// transfer buffer 1
 static void* lcd_transfer_buffer1 = NULL;
+// transfer buffer 2 (for DMA. See comments in setup())
 static void* lcd_transfer_buffer2 = NULL;
-
+// the htcw_uix object that manages screens
+// you can use a screen directly, and you might
+// if your app only has one, but the display object
+// allows for easily switching between different screens
 static uix::display lcd_display;
+// declare our screen type using the display's native pixel format
 using screen_t = uix::screen<rgb_pixel<16>>;
+// declare the label type with the screens "control surface" type
 using label_t = label<typename screen_t::control_surface_type>;
+// the color pseudo-enumartion for the screen's native pixel format
 using color_t = color<typename screen_t::pixel_type>;
 
 
@@ -136,46 +145,60 @@ static PAL_TYPE fire_palette[] = {
     RGB(31, 63, 30), RGB(31, 63, 30), RGB(31, 63, 31),
     RGB(31, 63, 31)};
 
+// it's common in UIX to write small controls in order to do demand draws when you 
+// need something a bit more involved that what the painter<> control allows for.
 template <typename ControlSurfaceType>
 class fire_box : public control<ControlSurfaceType> {
+    using base_type = control<control_surface_type>;
     int draw_state = 0;
     uint8_t p1[BUF_HEIGHT][BUF_WIDTH];  // VGA buffer, quarter resolution w/extra lines
     unsigned int i, j, k, l, delta;     // looping variables, counters, and data
     char ch;
-
    public:
+    // not necessarly strictly requires, but may be expected in larger 
+    // apps further down the chain, so they are strongly recommended
+    // when making generalized controls. Implemented here mainly
+    // for example
     using control_surface_type = ControlSurfaceType;
-    using base_type = control<control_surface_type>;
     using pixel_type = typename base_type::pixel_type;
     using palette_type = typename base_type::palette_type;
+    // this constructor not strictly necessary but recommended for generalized controls
     fire_box(uix::invalidation_tracker& parent, const palette_type* palette = nullptr)
         : base_type(parent, palette) {
     }
+    // required
     fire_box()
         : base_type() {
     }
+    // not strictly necessary but again recommended for generalized controls
     fire_box(fire_box&& rhs) {
         do_move_control(rhs);
         draw_state = 0;
     }
+    // not strictly necessary but recommended for generalized controls
     fire_box& operator=(fire_box&& rhs) {
         do_move_control(rhs);
         draw_state = 0;
         return *this;
     }
+    // not strictly necessary but recommended for generalized controls
     fire_box(const fire_box& rhs) {
         do_copy_control(rhs);
         draw_state = 0;
     }
+    // not strictly necessary but recommended for generalized controls
     fire_box& operator=(const fire_box& rhs) {
         do_copy_control(rhs);
         draw_state = 0;
         return *this;
     }
 protected:
+    // we require this to compute the frame, as this gets called ONCE
+    // per screen draw, whereas on_paint gets called potentially several
+    // times
     virtual void on_before_paint() override {
         switch (draw_state) {
-            case 0:
+            case 0: // first draw
                 // Initialize the buffer to 0s
                 for (i = 0; i < BUF_HEIGHT; i++) {
                     for (j = 0; j < BUF_WIDTH; j++) {
@@ -184,7 +207,7 @@ protected:
                 }
                 draw_state = 1;
                 // fall through
-            case 1:
+            case 1: // first draw and subsequent draws
                 // Transform current buffer
                 for (i = 1; i < BUF_HEIGHT; ++i) {
                     for (j = 0; j < BUF_WIDTH; ++j) {
@@ -219,6 +242,7 @@ protected:
                     }
                 }
                 delta = 0;
+                // make more 
                 for (j = 0; j < BUF_WIDTH; j++) {
                     if (rand() % 10 < 5) {
                         delta = (rand() & 1) * 255;
@@ -228,14 +252,18 @@ protected:
                 }
         }
     }
+    // here we implement the demand draw. The destination is an htcw_gfx draw target. The clip
+    // is the current rectangle within the destination that is actually being drawn, as this
+    // routine may be called multiple times to render the entire destination. 
+    // Note that the destination and clip use local coordinates, not screen coordinates.
     virtual void on_paint(control_surface_type& destination, const srect16& clip) override {
         for (int y = clip.y1; y <= clip.y2; ++y) {
 #ifdef USE_SPANS
         // must use rgb_pixel<16>
         static_assert(gfx::helpers::is_same<rgb_pixel<16>,typename screen_t::pixel_type>::value,"USE_SPANS only works with RGB565");
-        // get the spans for the current row
+        // get the span for the current partial row (starting at clip.x1)
         gfx_span row = destination.span(point16(clip.x1,y));
-        // get the pointer
+        // get the pointer to the partial row data
         uint16_t *prow = (uint16_t*)row.data;
 #endif
 
@@ -244,8 +272,10 @@ protected:
                 int j = x >> 2;
                 PAL_TYPE px = fire_palette[this->p1[i][j]];
  #ifdef USE_SPANS
+                // set the pixel
                 *(prow++)=px;
 #else
+                // set the pixel
                 destination.point(point16(x, y), px);
 #endif               
                 
@@ -254,90 +284,142 @@ protected:
     }
     
 };
+// declare the fire box type using the screen type's control surface type
 using fire_box_t = fire_box<typename screen_t::control_surface_type>;
 
-
-// for access to RGB8888 colors which controls use
+// for access to RGB8888 colors which htcw_uix controls use
 using color32_t = color<rgba_pixel<32>>;
 
+// callback for writing data to the display
 static void uix_on_flush(const rect16& bounds,
                              const void *bitmap, void* state) {
     lcd_flush(bounds.x1, bounds.y1, bounds.x2, bounds.y2, bitmap);
 }
+// flush_complete() must be called on the display object any time
+// a flush is done, even if it is synchronous (not using DMA).
+// Since we're using DMA, we get this lcd_on_flush_complete()
+// from the freenove driver, and let htcw_uix know we're done
+// flushing
 void lcd_on_flush_complete() {
     lcd_display.flush_complete();
 }
+// fonts are read from streams, so wrap a stream around our
+// embedded win 3.1 .fon file byte array
 static const_buffer_stream fps_font_stream(vga_8x8,sizeof(vga_8x8));
+// construct the font with the stream we just made
 static win_font fps_font(fps_font_stream);
+// the main screen where our controls reside
 static screen_t main_screen;
+// the FPS label which displays statistics
 static label_t fps_label;
+// the fire box control from above
 static fire_box_t main_fire;
 void setup() {
     Serial.begin(115200);
-
+    // we use two transfer buffers so htcw_uix can draw to one while sending
+    // the other at the same time to maximize throughput. Here we allocate both
     lcd_transfer_buffer1 = heap_caps_malloc(lcd_transfer_size, MALLOC_CAP_DMA);
     lcd_transfer_buffer2 = heap_caps_malloc(lcd_transfer_size, MALLOC_CAP_DMA);
     if (lcd_transfer_buffer1 == NULL || lcd_transfer_buffer2 == NULL) {
         puts("Could not allocate transfer buffers");
         ESP_ERROR_CHECK(ESP_ERR_NO_MEM);
     }
+    // initialize the devices
     lcd_initialize(lcd_transfer_size);
     touch_initialize(TOUCH_THRESH_DEFAULT);
+    // rotate the lcd and touch surface 90 degrees
     lcd_rotation(1);
     touch_rotation(1);
+    // set up the display object
+    // first the size of the transfer buffers:
+    // note that this is the size of each, not 
+    // the total for both.
     lcd_display.buffer_size(lcd_transfer_size);
+    // set the buffers. if we only used one,
+    // we would not take advantage of ESP32 DMA
     lcd_display.buffer1((uint8_t*)lcd_transfer_buffer1);
     lcd_display.buffer2((uint8_t*)lcd_transfer_buffer2);
+    // give the display object our flush callback
+    // so it can send bitmaps to the display
     lcd_display.on_flush_callback(uix_on_flush);
+    // set the screen dimensions (important!)
     main_screen.dimensions({320,240});
+    // defaults to black, but we set it anyway
+    // for example. Note that this is in the 
+    // *screen*'s color format, not htcw_uix's
+    // native color format, which is 32-bit RGBA
     main_screen.background_color(color_t::black);
+    // set the bounds for our custom fire control
+    // it takes up the whole screen
     main_fire.bounds(main_screen.bounds());
+    // register the control with the screen
     main_screen.register_control(main_fire);
+    // initialize the font
     fps_font.initialize();
+    // set the label color
     fps_label.color(color32_t::blue);
+    // set the bounds for the label (near the bottom)
     fps_label.bounds({0,230,319,238});
+    // want to align the text to the right
     fps_label.text_justify(uix_justify::top_right);
+    // set the font to use (from above)
     fps_label.font(fps_font);
+    // we don't want any padding around the text
     fps_label.padding({0,0});
+    // for now set the text to nothing - to be
+    // set later after we compute the FPS
     fps_label.text("");
+    // register the label with the screem
     main_screen.register_control(fps_label);
+    // tell the display object to use the screen
     lcd_display.active_screen(main_screen);
 
-    // led_initialize(); // conflicts with touch/i2c
+    // print the free ram
     printf("Free SRAM: %0.2fKB, free PSRAM: %0.2fMB\n",
            heap_caps_get_free_size(MALLOC_CAP_INTERNAL) / 1024.f,
            heap_caps_get_free_size(MALLOC_CAP_SPIRAM) / 1024.f / 1024.f);
 }
+// information for our FPS statistics:
 static int frames = 0;
 static uint32_t total_ms = 0;
 static uint32_t ts_ms = 0;
-static int col = 0;
 static char fps_buf[64];
 void loop() {
     uint32_t start_ms = pdTICKS_TO_MS(xTaskGetTickCount());
+    // force the fire control to repaint
     main_fire.invalidate();
+    // update the display - causes the frame to be 
+    // flushed to the display.
     lcd_display.update();
-    
+    // display any touch data
     uint16_t x, y;
     if (touch_xy(&x, &y)) {
         printf("touch: (%d, %d)\n", x, y);
     }
+    // when we have a flush pending, that means htcw_uix skipped a 
+    // frame due to the DMA being already tied up with a previous
+    // transaction. It will continue rendering once it is complete.
+    // However, we don't want to count a pending as one of the frames
+    // for calculating the FPS, since rendering was effectively skipped,
+    // or rather, delayed - no paint actually happened, yet.
     if(!lcd_display.flush_pending()) {
+        // compute the statistics
         ++frames;
         uint32_t end_ms = pdTICKS_TO_MS(xTaskGetTickCount());
 
         total_ms += (end_ms - start_ms);
         if (end_ms > ts_ms + 1000) {
             ts_ms = end_ms;
-            if (++col == 4) {
-                col = 0;
-            }
+            // make sure we don't div by zero
             if (frames > 0) {
                 sprintf(fps_buf, "FPS: %d, avg ms: %0.2f", frames,
                     (float)total_ms / (float)frames);
-                fps_label.text(fps_buf);
-                puts(fps_buf);
+            } else {
+                sprintf(fps_buf, "FPS: < 1, total ms: %d", (int)total_ms);
             }
+            // update the label (redraw is "automatic" (happens during lcd_display.update()))
+            fps_label.text(fps_buf);
+            puts(fps_buf);
             total_ms = 0;
             frames = 0;
         }
